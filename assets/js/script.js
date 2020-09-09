1bf0392f317aa6a5a93d2b77a668737b587bc713
luc1dLife <luc1d_life@protonmail.com>
2020-09-08 21:36:46 -0600

    Progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,35 +2,32 @@ var setDate = function () {
     $("#date").text(moment().format("L"));
 };
 
-var currentObj = [];
-var futureObj = [];
-
 var apiKey = "718523b17d4bbd2336cf57c34cc3836a";
 
 function getWeatherData(cityName) {
 
     var dataApi = "http://api.openweathermap.org/data/2.5/weather?q=Boise&appid=" + apiKey;
 
-    // Query Open Weather API to Get "coord", "weather"
+    // Query Open Weather API to Get "coord"
     fetch(dataApi)
         .then(function (response) {
             if (response.ok) {
-                response.json().then(function(data) {    
+                response.json().then(function(currentData) {    
 
-                    // Generate One Call Endpoint With Coordinates
-                    var lat = data.coord.lat;
-                    var lon = data.coord.lon;
+                    // Generate One Call Endpoint With Returned Coordinates
+                    var lat = currentData.coord.lat;
+                    var lon = currentData.coord.lon;
                     var forecastApi = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&appid=" + apiKey;
                     
                     fetch(forecastApi)
                     .then(function (response) {
                         if (response.ok) {
-                            response.json().then(function(data) {    
-                                var current = data.current;
-                                var daily = data.daily;
+                            response.json().then(function(forecastData) {    
+                                var current = currentData; // Result From dataApi
+                                var forecast = forecastData.daily; // Result From forecastApi
                                 
                                 // Send Data to be Compiled                              
-                                compileData(current, daily);
+                                compileData(current, forecast);
                             }
                             );
                         } else {
@@ -52,8 +49,17 @@ function getWeatherData(cityName) {
         });
 }
 
-var compileData = function(current, daily) {
-    console.log("daily:" + daily + "current:" + current);  
+var compileData = function(current, forecast) {
+    console.log("current:" + current + "forecast:" + forecast);
+    currentObj = {
+        cityName: current.name,
+        country: current.sys.country,
+    }
+
+    futureObj = {
+
+    }
+    console.log(currentObj.cityName + "," + currentObj.country);
 }
 
 var initial = function() {
